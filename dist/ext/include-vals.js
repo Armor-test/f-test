1e830311589292641f9aa963d1a17d63ee059e5c
ac-PragatiDubey <pragati.dubey@armorcode.io>
2025-08-27 00:20:24 +0530

    Implement substantive code improvements

--- dist/ext/include-vals.js
+++ dist/ext/include-vals.js
@@ -5,7 +5,7 @@
     }
 
     function mergeObjects(obj1, obj2) {
-        for (var key in obj2) {
+        for (const key in obj2) {
             if (obj2.hasOwnProperty(key)) {
                 obj1[key] = obj2[key];
             }
@@ -16,10 +16,10 @@
     htmx.defineExtension('include-vals', {
         onEvent: function (name, evt) {
             if (name === "htmx:configRequest") {
-                var includeValsElt = htmx.closest(evt.detail.elt, "[include-vals],[data-include-vals]");
+                const includeValsElt = htmx.closest(evt.detail.elt, "[include-vals],[data-include-vals]");
                 if (includeValsElt) {
-                    var includeVals = includeValsElt.getAttribute("include-vals") || includeValsElt.getAttribute("data-include-vals");
-                    var valuesToInclude = eval("({" + includeVals + "})");
+                    const includeVals = includeValsElt.getAttribute("include-vals") || includeValsElt.getAttribute("data-include-vals");
+                    const valuesToInclude = eval("({" + includeVals + "})");
                     mergeObjects(evt.detail.parameters, valuesToInclude);
                 }
             }
