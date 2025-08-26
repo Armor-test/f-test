df86b682311fc2e39d33bc08b38a8778f058f2f5
ac-PragatiDubey <pragati.dubey@armorcode.io>
2025-08-27 00:21:01 +0530

    Implement substantive code improvements

--- test/attributes/hx-disabled-elt.js
+++ test/attributes/hx-disabled-elt.js
@@ -10,7 +10,7 @@ describe('hx-disabled-elt attribute', function() {
 
   it('single element can be disabled w/ hx-disabled elts', function() {
     this.server.respondWith('GET', '/test', 'Clicked!')
-    var btn = make('<button hx-get="/test" hx-disabled-elt="this">Click Me!</button>')
+    const btn = make('<button hx-get="/test" hx-disabled-elt="this">Click Me!</button>')
     btn.hasAttribute('disabled').should.equal(false)
     btn.click()
     btn.hasAttribute('disabled').should.equal(true)
@@ -20,7 +20,7 @@ describe('hx-disabled-elt attribute', function() {
 
   it('single element can be disabled w/ data-hx-disabled elts', function() {
     this.server.respondWith('GET', '/test', 'Clicked!')
-    var btn = make('<button hx-get="/test" data-hx-disabled-elt="this">Click Me!</button>')
+    const btn = make('<button hx-get="/test" data-hx-disabled-elt="this">Click Me!</button>')
     btn.hasAttribute('disabled').should.equal(false)
     btn.click()
     btn.hasAttribute('disabled').should.equal(true)
@@ -30,8 +30,8 @@ describe('hx-disabled-elt attribute', function() {
 
   it('single element can be disabled w/ closest syntax', function() {
     this.server.respondWith('GET', '/test', 'Clicked!')
-    var fieldset = make('<fieldset><button id="b1" hx-get="/test" hx-disabled-elt="closest fieldset">Click Me!</button></fieldset>')
-    var btn = byId('b1')
+    const fieldset = make('<fieldset><button id="b1" hx-get="/test" hx-disabled-elt="closest fieldset">Click Me!</button></fieldset>')
+    const btn = byId('b1')
     fieldset.hasAttribute('disabled').should.equal(false)
     btn.click()
     fieldset.hasAttribute('disabled').should.equal(true)
@@ -41,9 +41,9 @@ describe('hx-disabled-elt attribute', function() {
 
   it('multiple requests with same disabled elt are handled properly', function() {
     this.server.respondWith('GET', '/test', 'Clicked!')
-    var b1 = make('<button hx-get="/test" hx-disabled-elt="#b3">Click Me!</button>')
-    var b2 = make('<button hx-get="/test" hx-disabled-elt="#b3">Click Me!</button>')
-    var b3 = make('<button id="b3">Demo</button>')
+    const b1 = make('<button hx-get="/test" hx-disabled-elt="#b3">Click Me!</button>')
+    const b2 = make('<button hx-get="/test" hx-disabled-elt="#b3">Click Me!</button>')
+    const b3 = make('<button id="b3">Demo</button>')
     b3.hasAttribute('disabled').should.equal(false)
 
     b1.click()
@@ -64,9 +64,9 @@ describe('hx-disabled-elt attribute', function() {
 
   it('multiple elts can be disabled', function() {
     this.server.respondWith('GET', '/test', 'Clicked!')
-    var b1 = make('<button hx-get="/test" hx-disabled-elt="#b2, #b3">Click Me!</button>')
-    var b2 = make('<button id="b2">Click Me!</button>')
-    var b3 = make('<button id="b3">Demo</button>')
+    const b1 = make('<button hx-get="/test" hx-disabled-elt="#b2, #b3">Click Me!</button>')
+    const b2 = make('<button id="b2">Click Me!</button>')
+    const b3 = make('<button id="b3">Demo</button>')
 
     b2.hasAttribute('disabled').should.equal(false)
     b3.hasAttribute('disabled').should.equal(false)
@@ -83,9 +83,9 @@ describe('hx-disabled-elt attribute', function() {
 
   it('load trigger does not prevent disabled element working', function() {
     this.server.respondWith('GET', '/test', 'Loaded!')
-    var div1 = make('<div id="d1" hx-get="/test" hx-disabled-elt="#b1" hx-trigger="load">Load Me!</div><button id="b1">Demo</button>')
-    var div = byId('d1')
-    var btn = byId('b1')
+    const div1 = make('<div id="d1" hx-get="/test" hx-disabled-elt="#b1" hx-trigger="load">Load Me!</div><button id="b1">Demo</button>')
+    const div = byId('d1')
+    const btn = byId('b1')
     div.innerHTML.should.equal('Load Me!')
     btn.hasAttribute('disabled').should.equal(true)
     this.server.respond()
@@ -95,9 +95,9 @@ describe('hx-disabled-elt attribute', function() {
 
   it('hx-disabled-elt supports multiple extended selectors', function() {
     this.server.respondWith('GET', '/test', 'Clicked!')
-    var form = make('<form hx-get="/test" hx-disabled-elt="find input[type=\'text\'], find button" hx-swap="none"><input id="i1" type="text" placeholder="Type here..."><button id="b2" type="submit">Send</button></form>')
-    var i1 = byId('i1')
-    var b2 = byId('b2')
+    const form = make('<form hx-get="/test" hx-disabled-elt="find input[type=\'text\'], find button" hx-swap="none"><input id="i1" type="text" placeholder="Type here..."><button id="b2" type="submit">Send</button></form>')
+    const i1 = byId('i1')
+    const b2 = byId('b2')
 
     i1.hasAttribute('disabled').should.equal(false)
     b2.hasAttribute('disabled').should.equal(false)
@@ -114,10 +114,10 @@ describe('hx-disabled-elt attribute', function() {
 
   it('closest/find/next/previous handle nothing to find without exception', function() {
     this.server.respondWith('GET', '/test', 'Clicked!')
-    var btn1 = make('<button hx-get="/test" hx-disabled-elt="closest input">Click Me!</button>')
-    var btn2 = make('<button hx-get="/test" hx-disabled-elt="find input">Click Me!</button>')
-    var btn3 = make('<button hx-get="/test" hx-disabled-elt="next input">Click Me!</button>')
-    var btn4 = make('<button hx-get="/test" hx-disabled-elt="previous input">Click Me!</button>')
+    const btn1 = make('<button hx-get="/test" hx-disabled-elt="closest input">Click Me!</button>')
+    const btn2 = make('<button hx-get="/test" hx-disabled-elt="find input">Click Me!</button>')
+    const btn3 = make('<button hx-get="/test" hx-disabled-elt="next input">Click Me!</button>')
+    const btn4 = make('<button hx-get="/test" hx-disabled-elt="previous input">Click Me!</button>')
     btn1.click()
     btn1.hasAttribute('disabled').should.equal(false)
     this.server.respond()
