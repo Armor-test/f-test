c3856758d85c85c1ff9b9d3d29d30ec327ccc16e
ac-PragatiDubey <pragati.dubey@armorcode.io>
2025-08-27 00:20:34 +0530

    Implement substantive code improvements

--- dist/ext/remove-me.js
+++ dist/ext/remove-me.js
@@ -4,7 +4,7 @@
             ".  It is recommended that you move to the version of this extension found on https://htmx.org/extensions")
     }
     function maybeRemoveMe(elt) {
-        var timing = elt.getAttribute("remove-me") || elt.getAttribute("data-remove-me");
+        const timing = elt.getAttribute("remove-me") || elt.getAttribute("data-remove-me");
         if (timing) {
             setTimeout(function () {
                 elt.parentElement.removeChild(elt);
@@ -15,12 +15,12 @@
     htmx.defineExtension('remove-me', {
         onEvent: function (name, evt) {
             if (name === "htmx:afterProcessNode") {
-                var elt = evt.detail.elt;
+                const elt = evt.detail.elt;
                 if (elt.getAttribute) {
                     maybeRemoveMe(elt);
                     if (elt.querySelectorAll) {
-                        var children = elt.querySelectorAll("[remove-me], [data-remove-me]");
-                        for (var i = 0; i < children.length; i++) {
+                        const children = elt.querySelectorAll("[remove-me], [data-remove-me]");
+                        for (const i = 0; i < children.length; i++) {
                             maybeRemoveMe(children[i]);
                         }
                     }
